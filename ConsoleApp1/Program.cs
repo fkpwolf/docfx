1e6ee919fed69aba3ee13d7140163d4bb9eaf41e
fkpwolf <fkpwolf@gmail.com>
2020-11-23 11:30:12 +0800

    try helloworld example

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            HelloWorldGenerated.HelloWorld.SayHello();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
